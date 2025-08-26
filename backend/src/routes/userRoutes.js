bd3a6629d7d55505906356ae5d362d993c90bb28
Angie Samson <97255743+AnzuWanzu@users.noreply.github.com>
2025-08-26 13:29:31 +0800

    Testing for signup route with jwt working. added nodemon and other packages

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { signup } from "../controllers/usersController";
+import { signup } from "../controllers/usersController.js";
 
 const router = express.Router();
 
---
+++ backend/src/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import userRoutes from "./routes/userRoutes.js";
+import dotenv from "dotenv";
+
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5002;
+
+//Middleware stuff
+app.use(express.json());
+
+//Routes
+app.use("/api/users/", userRoutes);
+
+//test route
+app.get("/", (req, res) => res.send("Time tracker api is running"));
+
+//DB connection
+connectDB().then(() => {
+  app.listen(PORT, () => console.log(`Server started on PORT: ${PORT}`));
+});
